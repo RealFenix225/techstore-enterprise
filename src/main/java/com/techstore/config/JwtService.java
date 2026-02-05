a62a2572c63537a0fbc8e85abdb969e46f14b644
Cesar Galvez <cesargalvez944@gmail.com>
2026-02-05 22:47:39 +0100

    Refactor: Seguridad implementada con variables de entorno

---
+++ src/main/java/com/techstore/config/JwtService.java
@@ -0,0 +1,79 @@
+package com.techstore.config;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Service;
+
+import java.security.Key;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.HexFormat;
+import java.util.Map;
+import java.util.function.Function;
+
+@Service
+public class JwtService {
+
+    //Inyectamos las variables desde application.properties
+    @Value("${application.security.jwt.secret-key}")
+    private String secretKey;
+
+    @Value("${application.security.jwt.expiration}")
+    private long jwtExpiration;
+
+    //1. GENERAR TOKEN (Solo con usuario)
+    public String generateToken(UserDetails userDetails){
+        return generateToken(new HashMap<>(), userDetails);
+    }
+
+    //2. GENERAR TOKEN (Con claims extra, por si quiero guardar el Rol dentro del token)
+    public String generateToken(Map<String, Object> extraClaims, UserDetails userDetails){
+        return Jwts.builder()
+                .setClaims(extraClaims)
+                .setSubject(userDetails.getUsername()) //Espacio para el email
+                .setIssuedAt(new Date(System.currentTimeMillis())) //Fecha creación
+                .setExpiration(new Date(System.currentTimeMillis() + jwtExpiration)) // Fecha fin
+                .signWith(getSignInKey(), SignatureAlgorithm.HS256) //Firma con el secreto
+                .compact();
+    }
+
+    //3. EXTRAER EL USERNAME (EMAIL) DEL TOKEN
+    public String extractUsername(String token) {
+        return extractClaim(token, Claims::getSubject);
+    }
+
+    // === Métodos auxiliares, lógica interna ===
+
+    public <T> T extractClaim(String token, Function<Claims, T> claimsResolver) {
+        final Claims claims = extractAllClaims(token);
+        return claimsResolver.apply(claims);
+    }
+
+    private Claims extractAllClaims(String token) {
+        return Jwts.parserBuilder()
+                .setSigningKey(getSignInKey()) //Usamos la misma llave para abrir la
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+    }
+
+    private boolean isTokenExpired(String token){
+        return extractExpiration(token).before(new Date());
+    }
+
+    private Date extractExpiration(String token){
+        return extractClaim(token, Claims::getExpiration);
+    }
+
+    private Key getSignInKey(){
+        //Esto decodifica la clave HEX qeue puse en properties
+        byte[] keyBytes = HexFormat.of().parseHex(secretKey);
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
+
+
+}
--- src/test/java/com/techstore/ApplicationTests.java
+++ src/test/java/com/techstore/ApplicationTests.java
@@ -2,12 +2,38 @@ package com.techstore;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import com.techstore.config.JwtService;
+import com.techstore.model.Role;
+import com.techstore.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
 
 @SpringBootTest
 class ApplicationTests {
+    @Autowired
+    private JwtService jwtService;
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+        // Nada aquí
+    }
+
+    @Test
+    void testTokenGeneration() {
+        // 1. Simular un usuario
+        User user = User.builder()
+                .email("general@techstore.com")
+                .password("password123") // No importa para el token
+                .role(Role.ADMIN)
+                .build();
+
+        // 2. Generar Token
+        String token = jwtService.generateToken(user);
+
+        // 3. Imprimir resultado
+        System.out.println("-----------------------------------------------------------");
+        System.out.println("TOKEN GENERADO CON ÉXITO:");
+        System.out.println(token);
+        System.out.println("-----------------------------------------------------------");
+    }
 
 }
